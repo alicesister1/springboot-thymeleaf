06a5666ac63a22d76d26a895b6c7d16589f3f6c3
KYUNGHEE YANG <alicesister1@gmail.com>
2022-02-15 00:42:42 +0900

    Thymeleaf 레이아웃

---
+++ src/main/java/com/example/thymeleaf/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/test")
+  public String index() {
+    return "index";
+  }
+}
